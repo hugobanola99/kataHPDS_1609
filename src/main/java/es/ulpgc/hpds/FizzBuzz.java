c3448a4b5ba55a27835f586156661ffe7f332102
hugobanola99 <hugo.banola101@alu.ulpgc.es>
2022-01-20 16:21:20 +0000

    Fizzbuzz test completed

--- src/main/java/es/ulpgc/hpds/FizzBuzz.java
+++ src/main/java/es/ulpgc/hpds/FizzBuzz.java
@@ -3,6 +3,32 @@ package es.ulpgc.hpds;
 public class FizzBuzz {
 
     public String value(int i) {
-        return "1";
+        if(isFizz(i)) {
+            if(isBuzz(i)){
+                return "fizzbuzz";
+            }else{
+                return "fizz";
+            }
+        }else if(isBuzz(i)){
+            return "buzz";
+        }else {
+            return String.valueOf(i);
+        }
+    }
+
+    public boolean isFizz(int n){
+        if(n % 3 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean isBuzz(int n){
+        if(n % 5 == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- src/test/java/es/ulpgc/hpds/FizBuzzTest.java
+++ src/test/java/es/ulpgc/hpds/FizBuzzTest.java
@@ -1,22 +1,62 @@
 package es.ulpgc.hpds;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizBuzzTest {
 
+    FizzBuzz fizzBuzz;
+
+    @Before
+    public void init(){
+        fizzBuzz = new FizzBuzz();
+    }
     @Test
     public void return_1_for_value_of_1(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String value = fizzBuzz.value(1);
-        assertEquals("1",value);
+
+        assertEquals("1",fizzBuzz.value(1));
     }
 
     @Test
     public void return_2_for_value_of_2(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String value = fizzBuzz.value(2);
-        assertEquals("2",value);
+
+        assertEquals("2",fizzBuzz.value(2));
+    }
+
+    @Test
+    public void return_fizz_for_value_of_3(){
+
+        assertEquals("fizz",fizzBuzz.value(3));
+    }
+
+    @Test
+    public void return_buzz_for_value_of_5(){
+        assertEquals("buzz",fizzBuzz.value(5));
+    }
+
+    @Test
+    public void return_fizz_for_value_of_6(){
+        assertEquals("fizz",fizzBuzz.value(6));
+    }
+    @Test
+    public void return_fizz_for_value_of_9(){
+        assertEquals("fizz",fizzBuzz.value(9));
+    }
+
+    @Test
+    public void return_buzz_for_value_of_10(){
+        assertEquals("buzz",fizzBuzz.value(10));
+    }
+
+    @Test
+    public void return_fizzbuzz_for_value_of_15(){
+        assertEquals("fizzbuzz",fizzBuzz.value(15));
+    }
+
+    @Test
+    public void return_fizzbuzz_for_value_of_30(){
+        assertEquals("fizzbuzz",fizzBuzz.value(30));
     }
 }
